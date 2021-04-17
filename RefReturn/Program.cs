fa110e2b55efeba273362600c67bab74f09c32f5
Gatsjy <gkswndks123@naver.com>
2021-04-17 21:51:36 +0900

    초기 커밋.

---
+++ RefReturn/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace RefReturn
+{
+    // 6.5 메서드의 결과를 참조로 반환하기
+    class Product
+    {
+        private int price = 100;
+
+        public ref int GetPrice()
+        {
+            return ref price;
+        }
+
+        public void PrintPrice()
+        {
+            Console.WriteLine($"Price : {price}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product carrot = new Product();
+            ref int ref_local_price = ref carrot.GetPrice();
+            int normal_local_price = carrot.GetPrice();
+
+            carrot.PrintPrice();
+            Console.WriteLine($"Ref Local Price : {ref_local_price}");
+            Console.WriteLine($"Normal_local_price : {normal_local_price}");
+
+            ref_local_price = 200;
+
+            carrot.PrintPrice();
+            Console.WriteLine($"Ref Local Price : {ref_local_price}");
+            Console.WriteLine($"Normal_local_price : {normal_local_price}");
+        }
+    }
+}
---
+++ chapter6_method/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace chapter6_method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
