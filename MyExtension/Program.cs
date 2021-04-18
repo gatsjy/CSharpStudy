0d8b6799bb046ed112956cd7081c3ccbbd2d1c59
Gatsjy <gkswndks123@naver.com>
2021-04-18 11:33:15 +0900

    2021-04-18, 한주안, 클래스 마무리

---
+++ MyExtension/Program.cs
@@ -0,0 +1,36 @@
+﻿using MyExtension;
+using System;
+
+namespace MyExtension
+{
+    public static class IntegerExtension
+    {
+        public static int Square(this int myInt)
+        {
+            return myInt * myInt;
+        }
+
+        public static int Power(this int MyInt, int exponent)
+        {
+            int result = MyInt;
+            for (int i = 1; i < exponent; i++)
+            {
+                result = result * MyInt;
+            }
+            return result;
+        }
+    }
+}
+
+namespace ExtensionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"3^2 : {3.Square()}");
+            Console.WriteLine($"3^2 : {3.Power(4)}");
+            Console.WriteLine($"3^2 : {2.Power(10)}");
+        }
+    }
+}
