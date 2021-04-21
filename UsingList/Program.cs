f344e6074a0873a46615d984fa29a97d1371f637
Gatsjy <gkswndks123@naver.com>
2021-04-21 22:28:12 +0900

    2021-04-21, 한주안, 여러컬렉션

---
+++ UsingList/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using static System.Console;
+
+namespace UsingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            for (int i = 0; i < 5; i++)
+            {
+                list.Add(i);
+            }
+
+            foreach (object obj in list)
+                Console.WriteLine(obj);
+            list.RemoveAt(2);
+
+            Queue q = new Queue();
+            q.Enqueue(1);
+            q.Enqueue(2);
+            q.Enqueue(3);
+            q.Enqueue(4);
+            q.Enqueue(5);
+
+            while(q.Count > 0)
+            {
+                Console.WriteLine(q.Dequeue());
+            }
+
+            Stack stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            while(stack.Count>0)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+
+            Hashtable ht = new Hashtable();
+            ht["하나"] = "one";
+            ht["둘"] = "two";
+            ht["셋"] = "three";
+            ht["넷"] = "four";
+            ht["다섯"] = "five";
+
+            WriteLine(ht["하나"]);
+            WriteLine(ht["둘"]);
+            WriteLine(ht["셋"]);
+        }
+    }
+}
