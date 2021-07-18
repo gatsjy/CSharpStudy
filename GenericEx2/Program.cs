fbc45dc42626b3724e2187dcfc5b58127ebd43ef
Gatsjy <gkswndks123@naver.com>
2021-07-18 14:00:53 +0900

    2021-07-18, 한주안, 제네릭스 관련 강의1

---
+++ GenericEx2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace GenericEx2
+{
+    // 일반화 클래스 선언 방식
+    // 일반화 클래스는 똑같은 기능을 하지만 내부적으로 사용하는 데이터 형식이 다를 경우에 사용하면 편리합니다.
+        /*
+           class 클래스명 <T>
+           {
+                
+           }
+
+           <예>
+           class DemoList
+           {
+                private int[] array;
+                
+                public int GetItem(int idx){ return array[idx] }
+           }
+
+           class DemoList2
+           {
+                private double[] array;
+
+                public int GetItem(double idx){ return array[idx] }
+           }
+
+           분리 된 두개의 클래스를... 내용은 같은데 자료형만 다르다??
+         */
+
+    class DemoList<T>
+    {
+        private T[] array;
+        public T GetItem(int idx) { return array[idx]; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoList<int> demoList1 = new DemoList<int>();
+            DemoList<Double> demoList2 = new DemoList<double>();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ GenericsEx/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace GenericsEx
+{
+    // Generics : int, float, double 과 같은 데이터 타입을 확정하지 않고 이 데이터 타입 자체를 
+    //타입 파라미터(Type Parameter) 받아들이도록 하는 기능
+    // 실수형으로 계산을 하게 될 때?
+    // 데이터 타입만 다르고 내부적으로 같은 로직일 뿐인데?? 이러한 불합리함을 제거하기 위해 제네릭스가 만들어졌다.
+    // C++의 템플릿 기능과 비슷한 개념이다!
+    // ** 성공하려면.. 남들 유투브 넷플릭스 보는 시간에 C# C++ C 를 공부해야 합니다!!
+    // 현대오토에버 가자!!
+    /*
+        일반화 메서드 선언 방식
+
+        접근제한자 반환형식 메소드이름<형식 매개 변수>(매개변수)
+        {
+        }
+
+        <예>
+        void CopyData(int a, int b)
+        {
+            b = a; // b에 a를 복사
+        }
+
+        void CopyData(string a, string b)
+        {
+            b = a; // b에 a를 복사... 문자열 a를 b에 복사
+        }
+
+        -> 이거 너무 비효율적인거 아닌가????
+        위의 방식은 데이터 형식에 따라서 여러개의 메소드가 필요하게된다.
+
+        void CopyData<T> (T a, T b) // 여기서 T는 Type을 뜻함
+        {
+            b = a;
+        }
+
+    */
+    class Program
+    {
+        static void CopyData<T> (T[] a, T[] b)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                b[i] = a[i];
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] a = { 11, 22, 33, 44, 55, };
+            int[] b = new int[a.Length];
+
+            CopyData<int>(a, b);
+            
+            foreach(int item in b)
+            {
+                Console.WriteLine(item);
+            }
+
+            string[] aa = { "사과", "배", "딸기", "포도", "수박" };
+            string[] bb = new string[aa.Length];
+
+            CopyData<string>(aa, bb);
+
+            foreach(string item in aa)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
