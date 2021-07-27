b7f8a3c1ac5bdae25ae91a600dcd27d3e6b31dc9
Gatsjy <gkswndks123@naver.com>
2021-07-27 22:40:56 +0900

    2021-07-27, 한주안, event에 대한 이해

--- DelegateDemo/Program.cs
+++ DelegateDemo/Program.cs
@@ -2,9 +2,11 @@
 
 namespace DelegateDemo
 {
+    // event는 delegate와 써야한다.
+    delegate void Transformer(int x);
+
     class Program
     {
-        delegate void Transformer(int x);
 
         static void Main(string[] args)
         {
@@ -19,6 +21,13 @@ namespace DelegateDemo
 
             t.Invoke(i); // invoking the delegate
 
+            NotifivationfMethods obj = new NotifivationfMethods();
+
+            obj.transformerEvent += User1.Xhandler;
+            obj.transformerEvent += User2.Yhandler;
+
+            obj.NotifyOnCell(i);
+
             Console.ReadLine();
         }
 
@@ -30,6 +39,34 @@ namespace DelegateDemo
         static void Cuber(int x)
         {
             Console.WriteLine(x * x * x);
+        }
+    }
+
+    class NotifivationfMethods
+    {
+        public event Transformer transformerEvent;
+
+        public void NotifyOnCell(int x)
+        {
+            if (transformerEvent != null)
+            {
+                transformerEvent(x);
+            }
+        }
+    }
+
+    class User1
+    {
+        public static void Xhandler(int x)
+        {
+            Console.WriteLine("Event received by User1 object");
+        }
+    }
+    class User2
+    {
+        public static void Yhandler(int x)
+        {
+            Console.WriteLine("Event received by User2 object");
         }
     }
 }
