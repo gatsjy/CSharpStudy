5fd86006649d25d3680c7d2c01aa119612b5e83a
Gatsjy <gkswndks123@naver.com>
2021-07-27 22:06:52 +0900

    2021-07-27, 한주안, c# delegate에 대한 새로운 이해

---
+++ DelegateDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DelegateDemo
+{
+    class Program
+    {
+        delegate void Transformer(int x);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int i = int.Parse(Console.ReadLine());
+
+            Transformer t; // create the instance
+
+            t = Square; // point to the function
+             
+            t += Cuber; // two methods till now
+
+            t.Invoke(i); // invoking the delegate
+
+            Console.ReadLine();
+        }
+
+        static void Square(int x)
+        {
+            Console.WriteLine(x * x);
+        }
+
+        static void Cuber(int x)
+        {
+            Console.WriteLine(x * x * x);
+        }
+    }
+}
