6db55eab3faa2a847d61a26d34bb666c450c0494
Gatsjy <gkswndks123@naver.com>
2021-07-28 22:42:24 +0900

    2021-07-28, 한주안, 어노니머스 메서드, 람다

---
+++ AnonymousMethodsandLambdaExpression/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AnonymousMethodsandLambdaExpression
+{
+    class Program
+    {
+        delegate void Transformer(int x);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int i = int.Parse(Console.ReadLine());
+
+            Transformer t; // create the instance;
+
+            //t = delegate (int x)
+            //{
+            //    Console.WriteLine(x * x);
+            //};
+
+            t = (int x) => { Console.WriteLine(x * x); };
+
+            t.Invoke(i);
+
+            Console.ReadLine();
+        }
+    }
+}
