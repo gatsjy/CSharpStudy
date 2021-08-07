8dee90a2eb3b0317ce4449f2f30a9802002a5d70
Gatsjy <gkswndks123@naver.com>
2021-08-07 21:58:07 +0900

    2021-08-07, 한주안, Thread 기본적인 예시

---
+++ ThreadTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadTest
+{
+    class ThreadTest
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(WriteY);
+            t.Start();
+
+            // 그와 동시에, 주 스레드도 나름의 작업을 진행한다.
+            for (int i = 0; i < 1000; i++) Console.Write("X");
+        }
+        static void WriteY()
+        {
+            for (int i = 0; i < 1000; i++) Console.Write("Y");
+        }
+    }
+}
