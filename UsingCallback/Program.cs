455fa9b3cf5962f3c9630d1650bce45867bc95f7
Gatsjy <gkswndks123@naver.com>
2021-08-08 15:30:21 +0900

    2021-08-08, 한주안, 제네릭 델리게이트

--- UsingCallback/Program.cs
+++ UsingCallback/Program.cs
@@ -2,39 +2,49 @@
 
 namespace UsingCallback
 {
-    delegate int Compare(int a, int b);
+    //delegate int Compare(int a, int b);
+    // 제네릭 버전
+    delegate int Compare<T>(T a, T b);
 
     class Program
     {
-        static int AscendCompare(int a, int b)
+        //static int AscendCompare<T>(int a, int b)
+        //{
+        //    if (a > b)
+        //        return 1;
+        //    else if (a == b)
+        //        return 0;
+        //    else
+        //        return -1;
+        //}
+        static int AscendCompare<T>(T a, T b) where T : IComparable<T>
         {
-            if (a > b)
-                return 1;
-            else if (a == b)
-                return 0;
-            else
-                return -1;
+            return a.CompareTo(b);
         }
 
-        static int DescendCompare(int a, int b)
+        //static int DescendCompare<T>(int a, int b)
+        //{
+        //    if (a < b)
+        //        return 1;
+        //    else if (a == b)
+        //        return 0;
+        //    else
+        //        return -1;
+        //}
+        static int DescendCompare<T>(T a, T b) where T : IComparable<T>
         {
-            if (a < b)
-                return 1;
-            else if (a == b)
-                return 0;
-            else
-                return -1;
+            return a.CompareTo(b) * -1;
         }
 
-        static void BubbleSort(int[] DataSet, Compare Comparer)
+        static void BubbleSort<T>(T[] DataSet, Compare<T> Comparer)
         {
             int i = 0;
             int j = 0;
-            int temp = 0;
+            T temp;
 
             for (i = 0; i < DataSet.Length - 1; i++)
             {
-                for (j = 0; j < DataSet.Length - (i+1); j++)
+                for (j = 0; j < DataSet.Length - (i + 1); j++)
                 {
                     if (Comparer(DataSet[j], DataSet[j + 1]) > 0)
                     {
@@ -50,7 +60,7 @@ namespace UsingCallback
         {
             int[] array = { 3, 7, 5, 2, 10 };
             Console.WriteLine("Sorting ascending");
-            BubbleSort(array, new Compare(AscendCompare));
+            BubbleSort(array, new Compare<int>(AscendCompare));
 
             for(int i = 0; i < array.Length; i++)
             {
@@ -59,12 +69,17 @@ namespace UsingCallback
 
             int[] array2 = { 7, 2, 8, 10, 11 };
             Console.WriteLine("\nSorting descending");
-            BubbleSort(array2, new Compare(DescendCompare));
+            BubbleSort(array2, new Compare<int>(DescendCompare));
 
             for (int i = 0; i < array2.Length; i++)
                 Console.Write("{0} ", array2[i]);
 
-            Console.WriteLine();
+            string[] array3 = { "abc", "def", "ghi", "jkl", "mno" };
+            Console.WriteLine("\nSorting descending");
+            BubbleSort(array3, new Compare<string>(DescendCompare));
+
+            for (int i = 0; i < array3.Length; i++)
+                Console.Write("{0} ", array3[i]);
         }
     }
 }
