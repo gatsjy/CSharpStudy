cb411d1adc18806b986ba4cce8a53f243d65cef0
Gatsjy <gkswndks123@naver.com>
2021-08-08 21:33:25 +0900

    2021-08-08, 한주안, C# 공부

---
+++ Async/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    class Program
+    {
+        async static private void MyMethodAsync(int count)
+        {
+            Console.WriteLine("C");
+            Console.WriteLine("D");
+
+            await Task.Run(async () =>
+            {
+                for (int i = 0; i <= count; i++)
+                {
+                    Console.WriteLine("{0}/{1} ...", i, count);
+                    await Task.Delay(100); // Thread.Sleep() 의 비동기 버전
+                }
+            });
+
+            Console.WriteLine("G");
+            Console.WriteLine("H");
+        }
+
+        static void Caller()
+        {
+            Console.WriteLine("A");
+            Console.WriteLine("B");
+
+            MyMethodAsync(3);
+
+            Console.WriteLine("E");
+            Console.WriteLine("F");
+        }
+        static void Main(string[] args)
+        {
+            Caller();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CallerInfo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace CallerInfo
+{
+    public static class Trace
+    {
+        public static void WriteLine(string message,
+            [CallerFilePath] string file = "",
+            [CallerLineNumber] int line = 0,
+            [CallerMemberName] string member = "")
+        {
+            Console.WriteLine("{0}(Line:{1}) {2}: {3}", file, line, member, message);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trace.WriteLine("즐거운 프로그래밍");
+        }
+    }
+}
