98bb610e61bbc78ded59861fdcafc46adc96482f
Gatsjy <gkswndks123@naver.com>
2022-03-06 11:24:51 +0900

    2022-03-06, 한주안, 쓰레드 공부

---
+++ arrayex1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace arrayex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = {{0,0,4,1,0},{0,2,0,0,0},{5,0,0,0,0},{0,0,2,0,3},{4,0,0,0,0}};
+
+            // 1. 행 우선 방식으로 할당
+            int[,] b = new int[5, 5];
+            for(int i = 0; i < 5; i++)
+            {
+                for(int j = 0; j < 5; j++)
+                {
+                    b[i,j] = a[i,j];
+                }
+            }
+            for(int i = 0; i < 5; i++)
+            {
+                for(int j = 0; j < 5; j++)
+                {
+                    Console.Write(b[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("B[2][4] : {0}", b[2,4]);
+
+            // 1. 열 우선 방식으로 할당
+            int[,] c = new int[5, 5];
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    c[i, j] = a[j, i];
+                }
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(c[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("C[2][4] : {0}", c[2, 4]);
+        }
+    }
+}
---
+++ makingThread/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+
+namespace makingThread
+{
+    public class ServerClass
+    {
+        // The method that will be called when the thread is started
+        public void InstanceMethod()
+        {
+            Console.WriteLine("ServerClass.InstanceMethod 다른 쓰레드에서 실행 중");
+
+            // Pause for a moment to provide a delay to make threads more apparent.
+            Thread.Sleep(3000);
+            Console.WriteLine("The instance method called by the worker 쓰레드 끝");
+        }
+
+        public static void StaticMethod()
+        {
+            Console.WriteLine("ServerClass.StaticMethod 다른 쓰레드에서 실행 중");
+
+            // Pause for a moment to provide a delay to make threads more apparent
+            Thread.Sleep(5000);
+            Console.WriteLine(
+            "The static method called by the worker thread 쓰레드 끝");
+        }
+    }
+
+    public class Simple
+    {
+        public static void Main()
+        {
+            ServerClass serverObject = new ServerClass();
+
+            // Create the thread object, passing in the
+            // serverObject.InstanceMethod method using a
+            // ThreadStart delegate.
+            Thread InstanceCaller = new Thread(new ThreadStart(serverObject.InstanceMethod));
+
+            // Start the Thread
+            InstanceCaller.Start();
+
+            Console.WriteLine("The Main() thread this after " + "starting the new InstanceCaller thread.");
+
+            // Create the thread object, passing in the
+            // serverObject.StaticMethod method using a
+            // ThreadStart delegate.
+            Thread StaticCaller = new Thread(
+                new ThreadStart(ServerClass.StaticMethod));
+
+            // Start the thread.
+            StaticCaller.Start();
+
+            Console.WriteLine("The Main() thread calls this after "
+                + "starting the new StaticCaller thread.");
+        }
+    }
+}
