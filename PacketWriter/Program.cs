06faf683f93b9ce009cc0499bde29460e1ce8024
Gatsjy <gkswndks123@naver.com>
2022-03-14 23:59:37 +0900

    2022-03-14, 한주안, MemoryWriter

---
+++ PacketWriter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace PacketWriter
+{
+    public class PacketWriter : BinaryWriter
+    {
+        private MemoryStream _ms;
+
+        public PacketWriter()
+            : base()
+        {
+            _ms = new MemoryStream();
+            OutStream = _ms;
+        }
+
+        public byte[] GetBytes()
+        {
+            Close();
+
+            byte[] data = _ms.ToArray();
+
+
+        }
+    }
+}
